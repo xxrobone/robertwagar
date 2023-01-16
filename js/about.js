412eb851958b6d6b26c706122f8a594486925c84
robonexx <robertwagar@gmail.com>
2023-01-16 21:20:02 +0100

    about page

--- js/about.js
+++ js/about.js
@@ -2,12 +2,9 @@ let bg = document.querySelector('.img_wrapper');
 let img = document.querySelector('.hero_img');
 
 bg.addEventListener('mouseenter', () => {
-    img.classList.add('img_animation')
-    img.src = '../assets/images/portfolio_img.png';
-    
+  img.src = '../assets/images/portfolio_img.png';
 });
 
 bg.addEventListener('mouseleave', () => {
-    img.src = '../assets/images/portfolio_img_bw.png';
-    img.classList.remove('img_animation')
+  img.src = '../assets/images/portfolio_img_bw.png';
 });
