d86229582c1071ed8bbdf08419fec84a1949b984
robonexx <robertwagar@gmail.com>
2023-01-17 14:33:55 +0100

    saving vistor in localstorage using it to show different messages

--- js/nameform.js
+++ js/nameform.js
@@ -1,17 +1,30 @@
 let form = document.querySelector('.input_form');
+let userName = JSON.parse(localStorage.getItem('userName'));
 
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
-  let name = document.querySelector('.input_name').value;
+let answer = document.querySelector('.answer');
+let name = '';
+
+console.log('username from ls ' + userName);
+
+if (!userName || userName === undefined || userName === '') {
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    name = document.querySelector('.input_name').value;
+    showMsg(name);
+    console.log('name from submit ' + name);
+  });
+} else {
   form.remove();
+  answer.textContent = `Hi again ${userName}, how can I help you today? :D`;
+}
 
-  let answer = document.querySelector('.answer');
-
+function showMsg(name) {
   if (name === '' || name === null || name === undefined) {
+    form.remove();
     answer.textContent = `Hi there Friend, nice to meet you :D`;
   } else {
+    form.remove();
     answer.textContent = `Hi there ${name}, nice to meet you :D`;
+    localStorage.setItem('userName', JSON.stringify(name));
   }
-  
-  console.log(name);
-});
+}
