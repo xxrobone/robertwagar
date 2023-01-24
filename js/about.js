21bf634ee51db8fac78a4814884c0a6a9fafb71d
robonexx <robertwagar@gmail.com>
2023-01-24 15:25:02 +0100

    about img fix and listener on scroll

--- js/about.js
+++ js/about.js
@@ -1,10 +1,16 @@
-let bg = document.querySelector('.img_wrapper');
-let img = document.querySelector('.hero_img');
-
+let bg = document.querySelector('.hero');
+/* let img = document.querySelector('.hero_img'); */
+/* 
 bg.addEventListener('mouseenter', () => {
   img.src = '../assets/images/portfolio_img.png';
 });
 
 bg.addEventListener('mouseleave', () => {
   img.src = '../assets/images/portfolio_img_bw.png';
+});
+ */
+
+window.addEventListener('scroll', () => {
+  bg.style.backgroundSize = 160 - +window.pageYOffset / 12 + '%';
+  bg.style.opacity = 1 - +window.pageYOffset / 700 + '';
 });
